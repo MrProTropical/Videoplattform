be9def43692b95f44e1f43dff4e38115f3cd97ec
Uch <79190681+Uch1243@users.noreply.github.com>
2021-05-03 12:49:54 +0200

    Add files via upload
    in base.html ist das layout der seite, dass überall gleich ist und in index.html wird dieses layout mit dem seiteninhalt erweitert

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ def index():
                 return render_template('index.html', username = session['username'])
     return render_template('index.html')
 
-
 @app.route('/signup', methods=['GET','POST'])
 def signup():
     if request.method == 'GET':
